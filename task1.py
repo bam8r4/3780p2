98bf9f1b29ff79b4b59b6295a6e1c8f84731099d
Brent Moran <brentmoran@Brents-MacBook-Pro-2.local>
2020-11-08 20:31:05 -0600

    Added plenty of changes to task1.py added salts and input validation

--- task1.py
+++ task1.py
@@ -1,14 +1,35 @@
 import hashlib
 import sys
+import random
 
 def verifyUserName(userName):
     file = open("file1","r")
+
+    if(userName.isalpha()):
+        if(len(userName) != 10):
+            print("Username is not an appropriate length. Must be 10 characters long.")
+            return False
+    else:
+        print("Username must be alphabetical characters only.")
+        return False
 
     for line in file:
         uN = line.split(',')
         if(userName == uN[0]):
             print("That username is already taken.")
             return False
+
+    return True
+
+def verifyPassword(password):
+    if(password.isdecimal()):
+        return True
+    else:
+        print("Password can be any length but must be all integers.")
+        return False
+
+def generateSalt():
+    return random.randint(0,10)
 
 
 # initializing string
@@ -18,9 +39,17 @@ while(again):
     answer = input("Enter c to create and account, l to login, or any other value to quit.")
     if(answer == 'c'):
         validUser = False
+        validPassword = False
+
         while(validUser == False):
             userName = input("Please enter a username: ")
             validUser = verifyUserName(userName)
+
+        while(validPassword == False):
+            password = input("Please enter a password made of only integers: ")
+            validPassword = verifyPassword(password)
+
+        salt = generateSalt()
 
     elif(answer == 'l'):
         print("loggin in")
