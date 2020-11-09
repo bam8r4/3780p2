c8a78f5a32d699a4ca6529bbb4c8ef088cacc8f5
bam8r <bam8r4@mst.edu>
2020-11-08 23:49:20 -0600

    updating some files

---
+++ task3.py
@@ -0,0 +1,11 @@
+import hashlib
+import sys
+import random
+
+#Creating this boolean to tell the code whether we are using file type 2 or file type 3
+type2 = True
+
+def increment(password):
+    
+
+password = "0"
